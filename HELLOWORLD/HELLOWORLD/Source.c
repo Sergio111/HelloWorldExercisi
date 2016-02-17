c926eeaf9d27fc232b5a093236f53abfc2309f34
nicobabot <nico.babotaltisen@gmail.com>
2016-02-17 17:06:50 +0100

    Helloworld
    Changed by nico

--- HELLOWORLD/HELLOWORLD/Source.c
+++ HELLOWORLD/HELLOWORLD/Source.c
@@ -3,7 +3,7 @@
 int main(){
 
 	printf("Hello world I'm Sergio from Barcelona");
-
+	printf("Hello world I'm Nico from Sant Cugat");
 	printf("Hello world I'm Xavi from Terrassa");
 	printf("Hello world I'm Daniel from Gracia, Barcelona");
 
